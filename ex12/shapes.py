519dfa553faec2f25351b8a8a44eff685dcebac8
Avinoam <avinoam@hershler.com>
2019-01-14 20:27:04 +0200

    docs

--- ex12/shapes.py
+++ ex12/shapes.py
@@ -10,8 +10,8 @@ class Shapes:
         :param light_source:A Point3D object with the light-source location.
         :param filename: A string with the past to an .OBJ file with a 3D
                          object.
-        :param color:
-        :param tag:
+        :param color: rgb values for this object's color
+        :param tag: A String with this object's unique tag.
         """
         self.__magoz = magoz
         self.light_source = light_source
@@ -19,45 +19,52 @@ class Shapes:
         self.light_source = self.light_source.get_points() / np.linalg.norm(
             self.light_source.get_points())
         self.filename = filename
-        self.__needs_update = True
         self.__tag = tag
+
+        # Set the flag to update positions:
+        self.__needs_update = True
+
+
+        # Initiate all the needed lists as empty:
         self.__num_vertices = 0
         self.__num_faces = 0
-        self.x_real = []
-        self.y_real = []
-        self.z_real = []
-
-        self.__guf_order = []
-        self.x_guf = []  # [[0 for i in range(num_vertices)] for j in
-        # range(num_facets)]
-        self.y_guf = []  # [[0 for i in range(num_vertices)] for j in
-        # range(num_facets)]
-        self.z_guf = []  # [[0 for i in range(num_vertices)] for j in
-        # range(num_facets)]
+        self.x_vertices = []
+        self.y_vertices = []
+        self.z_vertices = []
+        self.__faces_order = []
+        self.x_face = []
+        self.y_face = []
+        self.z_face = []
         self.__colors = []
         self.__shades = []
         self.__disp = []
-
         self.__vertices = []
         self.__faces = []
 
     def build_shape(self, x, y, z):
+        """
+        Build the shape in the given coordinates.
+        :param x: The x coordinate to build in.
+        :param y: The y coordinate to build in.
+        :param z: The z oordinate to build in.
+        :return:
+        """
         self.__build_from_file()
         for vertice in self.__vertices:
             xv, yv, zv = vertice.split(' ')
-            self.x_real.append(float(xv) + x)
-            self.y_real.append(float(yv) + y)
-            self.z_real.append(float(zv) + z)
-        self.__guf_order = [i for i in range(len(self.__faces))]
+            self.x_vertices.append(float(xv) + x)
+            self.y_vertices.append(float(yv) + y)
+            self.z_vertices.append(float(zv) + z)
+        self.__faces_order = [i for i in range(len(self.__faces))]
         # self.real_to_guf()
         # self.__build_normals()
 
     def real_to_guf(self):
         if not self.__needs_update:
             return
-        self.x_guf = []
-        self.y_guf = []
-        self.z_guf = []
+        self.x_face = []
+        self.y_face = []
+        self.z_face = []
         self.__colors = []
         red, green, blue = self.__color
         for face in self.__faces:
@@ -65,16 +72,16 @@ class Shapes:
             a = int(a)
             b = int(b)
             c = int(c)
-            x = [self.x_real[a - 1], self.x_real[b - 1],
-                 self.x_real[c - 1]]
-            y = [self.y_real[a - 1], self.y_real[b - 1],
-                 self.y_real[c - 1]]
-            z = [self.z_real[a - 1], self.z_real[b - 1],
-                 self.z_real[c - 1]]
+            x = [self.x_vertices[a - 1], self.x_vertices[b - 1],
+                 self.x_vertices[c - 1]]
+            y = [self.y_vertices[a - 1], self.y_vertices[b - 1],
+                 self.y_vertices[c - 1]]
+            z = [self.z_vertices[a - 1], self.z_vertices[b - 1],
+                 self.z_vertices[c - 1]]
 
-            self.x_guf.append(x)
-            self.y_guf.append(y)
-            self.z_guf.append(z)
+            self.x_face.append(x)
+            self.y_face.append(y)
+            self.z_face.append(z)
             n = np.cross((x[0], y[0], z[0]), (x[1], y[1], z[1]))
             n = n / np.linalg.norm(n)
             shade = np.dot((n[0], n[1], n[2]), self.light_source)
@@ -84,13 +91,13 @@ class Shapes:
                                    int(green - 0.5 * abs(shade) * green),
                                    int(blue - 0.5 * abs(shade) * blue)))
 
-        self.__guf_order.sort(key=lambda value: min(self.z_guf[value]),
-                              reverse=True)
+        self.__faces_order.sort(key=lambda value: min(self.z_face[value]),
+                                reverse=True)
         self.__disp = []
         for i in range(len(self.__faces)):
-            self.__convert(self.x_guf[self.__guf_order[i]],
-                           self.y_guf[self.__guf_order[i]],
-                           self.z_guf[self.__guf_order[i]])
+            self.__convert(self.x_face[self.__faces_order[i]],
+                           self.y_face[self.__faces_order[i]],
+                           self.z_face[self.__faces_order[i]])
 
     # self.guf_order = [i for i in range(self.__num_faces)]
 
@@ -110,8 +117,8 @@ class Shapes:
                 self.__disp.append((x[i], y[i]))
 
     def convert_now(self):
-        for i in range(len(self.x_guf)):
-            self.__convert(self.x_guf, self.y_guf, self.z_guf)
+        for i in range(len(self.x_face)):
+            self.__convert(self.x_face, self.y_face, self.z_face)
 
     def __build_from_file(self):
         self.__num_vertices = 0
@@ -132,27 +139,27 @@ class Shapes:
 
     def get_big_z(self, z=None):
         if not z:
-            z = self.z_real
+            z = self.z_vertices
         return max(z)
 
     def get_small_z(self):
-        return min(self.z_real)
+        return min(self.z_vertices)
 
     def get_big_y(self, y=None):
         if not y:
-            y = self.y_real
+            y = self.y_vertices
         return max(y)
 
     def get_small_y(self):
-        return min(self.y_real)
+        return min(self.y_vertices)
 
     def get_big_x(self, x=None):
         if not x:
-            x = self.x_real
+            x = self.x_vertices
         return max(x)
 
     def get_small_x(self):
-        return min(self.x_real)
+        return min(self.x_vertices)
 
     def get_middle(self):
         x = (self.get_big_x() + self.get_small_x()) / 2
@@ -175,21 +182,21 @@ class Shapes:
         for i in range(self.__num_faces):
 
             # TODO: Tidy
-            if not self.__check(*self.__disp[self.__guf_order[i]][0],
-                                *self.__disp[self.__guf_order[i]][1],
-                                *self.__disp[self.__guf_order[i]][2]):
-                color = self.__colors[self.__guf_order[i]]
-                canvas.create_polygon(self.__disp[self.__guf_order[i]],
+            if not self.__check(*self.__disp[self.__faces_order[i]][0],
+                                *self.__disp[self.__faces_order[i]][1],
+                                *self.__disp[self.__faces_order[i]][2]):
+                color = self.__colors[self.__faces_order[i]]
+                canvas.create_polygon(self.__disp[self.__faces_order[i]],
                                       fill=color, tag=tag)
         self.__needs_update = False
 
     def mull_points(self, matrix):
         if matrix.check_identity():
             return
-        self.x_real, self.y_real, self.z_real = matrix.mullAllPoints(
-            self.x_real,
-            self.y_real,
-            self.z_real,
+        self.x_vertices, self.y_vertices, self.z_vertices = matrix.mullAllPoints(
+            self.x_vertices,
+            self.y_vertices,
+            self.z_vertices,
             self.__num_vertices)
         self.__needs_update = True
 
